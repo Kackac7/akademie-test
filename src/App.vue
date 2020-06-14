6a3cd5d128be41f39bf59c2e2b566fd1ddf03af9
Kackac7 <65717675+Kackac7@users.noreply.github.com>
2020-06-15 00:09:26 +0200

    test

--- src/App.vue
+++ src/App.vue
@@ -3,48 +3,54 @@
 
     <h1>Dad Jokes</h1>
 
-    <!--
-      Toto je HTML šablona pro jeden vtip.
-      Pro vtip ale chceme mít samostatnou komponentu nazvanou Joke.
-      Vytvoř komponentu, vezmi HTML zakomentované níže a vlož ho
-      dovnitř template komponenty.
-
-      Do komponenty posílej potřebné props a jejich hodnoty zobrazuj
-      místo textu vtipů a počtů lajků, které jsou teď v šabloně
-      zadané natvrdo. Nezapomeň i na ikonku uživatele.
-
-      Zde uvnitř App vue pak procházej pole vtipů pomocí v-for
-      a zobrazuj komponentu Joke pro každý vtip na seznamu.
-
-      Zařiď, aby tlačítka v komponentě fungovala a emitovala sem
-      do App.vue události, které zde budou spouštět příslušné metody,
-      které změní počty lajků u vtipu v poli.
-    -->
-    <!--
-    <div class="joke">
-      <div class="joke-body">
-        <img class="joke-user" src="/images/users/user01.png" />
-        <p class="joke-text">
-          The secret service isn't allowed to yell "Get down!" anymore when the president is about to be attacked. Now they have to yell "Donald, duck!"
-        </p>
-      </div>
-      <div class="joke-likes">
-        <button class="btn-like btn-up"></button>
-        <span class="likes-count likes-up">0</span>
-
-        <button class="btn-like btn-down"></button>
-        <span class="likes-count likes-down">0</span>
-      </div>
-    </div>
-    -->
-
+   <joke 
+    v-for="joke in jokes"
+   v-bind:key="joke.id"
+   v-bind:id="joke.id"
+   v-bind:vtip="joke.vtip"
+   v-bind:user="joke.user"
+   v-bind:like="joke.like"
+   v-bind:dislike="joke.dislike"
+   v-bind:give-like="dajLike($event)"
+   v-bind:give-dislike="dajDislike($event)"
+/>
   </div>
 </template>
 
 
 <script>
-export default {
+import Joke from "./components/Joke.vue";
 
+export default {
+  components: {
+    joke : Joke
+  },
+  data() {
+    return {
+      jokes: [
+      { id: '1', vtip: 'The secret service is not allowed to yell "Get down!" anymore when the president is about to be attacked. Now they have to yell "Donald, duck!"', user: 'user01', like: '13', dislike: '24' },
+    { id: '2', vtip: 'Why did the invisible man turn down the job offer? He could not see himself doing it.', user: 'user02', like: '50', dislike: '10' },
+    { id: '3', vtip: 'I bought some shoes from a drug dealer. I dont know what he laced them with, but I was tripping all day!', user: 'user03', like: '30', dislike: '2' },
+    { id: '4', vtip: 'Did you hear the news? FedEx and UPS are merging. They are going to go by the name Fed-Up from now on.', user: 'user04', like: '15', dislike: '8' },
+    { id: '5', vtip: 'My daughter screeched, "Daaaaaad, you have not listened to one word I have said, have you!?" What a strange way to start a conversation with me...', user: 'user05', like: '90', dislike: '44' },
+    { id: '6', vtip: 'My wife tried to unlatch our daughters car seat with one hand and said, "How do one armed mothers do it?" Without missing a beat I replied, "Single handedly."', user: 'user06', like: '38', dislike: '1' },
+    { id: '7', vtip: 'When a dad drives past a graveyard: Did you know that is a popular cemetery? Yep, people are just dying to get in there!', user: 'user07', like: '66', dislike: '35' },
+    { id: '8', vtip: 'My friend keeps saying "cheer up man it could be worse, you could be stuck underground in a hole full of water." I know he means well.', user: 'user08', like: '27', dislike: '18' },
+    { id: '9', vtip: 'The fattest knight at King Arthurs round table was Sir Cumference. He acquired his size from too much pi.', user: 'user09', like: '42', dislike: '9' },
+    ]
+    }
+  },
+  methods: {
+    zmenLike(id) {
+      const likes = this.jokes.find(joke => joke.id === id);
+      likes.like ++;
+    },
+
+    zmenDislike(id) {
+      const dislikes = this.jokes.find(joke => joke.id === id);
+      dislikes.dislike ++;
+    }
+  },
 }
 </script>
 
---
+++ src/components/Joke.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="joke">
+      <div class="joke-body">
+        <img class="joke-user" v-bind:src="`/images/users/${user}.png`"/>
+        <p class="joke-text"> {{vtip}}</p>
+      </div>
+      <div class="joke-likes">
+        <button class="btn-like btn-up" v-on:click="zmenLike"></button>
+        <span class="likes-count likes-up">{{like}}</span>
+
+        <button class="btn-like btn-down" v-on:click="zmenDislike"></button>
+        <span class="likes-count likes-down">{{dislike}}</span>
+      </div>
+    </div>
+</template>
+<script>
+
+export default {
+    
+    props: ['id', 'vtip', 'user', 'like', 'dislike'],
+
+methods: {
+        dajLike() {
+            this.$emit('give-like', this.id)
+        },
+        dajDislike() {
+            this.$emit('give-dislike', this.id)
+        }
+}
+}
+</script>
+<style>
+
+</style>
